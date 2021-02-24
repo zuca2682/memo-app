e012f31aca381fa0263d26016a368c1fc3234a89
zuca <2682zucazuca@gmail.com>
2021-02-24 14:24:55 +0900

    Vue.jsの実装

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -5,6 +5,11 @@
         {{ memo.title }}: {{ memo.description }}
       </li>
     </ul>
+    <div>
+      <input v-model="title" placeholder="title">
+      <input v-model="description" placeholder="description">
+      <button @click="addMemo">メモを追加</button>
+    </div>
   </div>
 </template>
 
@@ -14,7 +19,9 @@
 export default {
   data: function () {
     return {
-      memos: "memos"
+      memos: "memos",
+      title: '',
+      description: '',
     }
   },
   mounted () {
@@ -26,6 +33,15 @@ export default {
       .then(response => (
         this.memos = response.data
       ))
+    },
+    addMemo: function () {
+      axios.post('/api/memos',{
+        title: this.title,
+        description: this.description
+      })
+      .then(response => (
+        this.setMemo()
+      ));
     }
   }
 }
